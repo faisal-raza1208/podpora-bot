67d647b53627b2aed46c7b9367af14a0b8b75914
Marek L <nospam.keram@gmail.com>
2020-06-20 18:19:32 +0100

    Add timestamps in logs

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -2,8 +2,15 @@ import winston from 'winston';
 
 const options: winston.LoggerOptions = {
     transports: [
-        new winston.transports.File({ filename: 'log/debug.log', level: 'debug' })
-    ]
+        new winston.transports.File({
+            filename: 'log/debug.log',
+            level: 'debug'
+        })
+    ],
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    )
 };
 
 const logger = winston.createLogger(options);
