69c3000f6b6bd63adb2af21a78ec025901974690
Marek L <nospam.keram@gmail.com>
2020-06-29 21:22:46 +0100

    Make sure lib directory is included in test coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,35 +1,36 @@
 module.exports = {
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.json'
-    }
-  },
-  moduleFileExtensions: [
-    'ts',
-    'js'
-  ],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'
-  },
-  testMatch: [
-    '**/test/**/*.test.(ts|js)'
-  ],
-  testEnvironment: 'node',
-  testTimeout: 250,
-  verbose: false,
-  notify: true,
-  notifyMode: 'always',
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
-    }
-  },
-  collectCoverageFrom: [
-    'src/app.ts',
-    'src/controllers/**/*.ts',
-    'src/models/**/*.ts'
-  ]
+    globals: {
+        'ts-jest': {
+            tsConfig: 'tsconfig.json'
+        }
+    },
+    moduleFileExtensions: [
+        'ts',
+        'js'
+    ],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest'
+    },
+    testMatch: [
+        '**/test/**/*.test.(ts|js)'
+    ],
+    testEnvironment: 'node',
+    testTimeout: 250,
+    verbose: false,
+    notify: true,
+    notifyMode: 'always',
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100
+        }
+    },
+    collectCoverageFrom: [
+        'src/app.ts',
+        'src/controllers/**/*.ts',
+        'src/models/**/*.ts',
+        'src/lib/**/*.ts'
+    ]
 };
