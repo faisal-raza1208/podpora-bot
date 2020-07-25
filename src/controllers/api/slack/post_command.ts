ad0887f6c3c982084c80352577cda336e026d828
Marek L <nospam.keram@gmail.com>
2020-07-25 10:17:00 +0100

    Inline postCommandHandler

--- src/controllers/api/slack/post_command.ts
+++ src/controllers/api/slack/post_command.ts
@@ -9,27 +9,24 @@ import {
     PostCommandPayload
 } from '../../../lib/slack/api_interfaces';
 
-function postCommandHandler(payload: PostCommandPayload, res: Response): Response {
-    const { team_id, command } = payload;
-    const slack_config = store.slackTeamConfig(team_id);
-    const slack = new Slack(slack_config);
-
-    if (command === '/support') {
-        return support.handleCommand(slack, payload, res);
-    }
-
-    return res.json({
-        text: 'Unknown or not implemented command.'
-    });
-}
-
 /**
  * POST /api/slack/command
  *
  */
 export const postCommand = (req: Request, res: Response): void => {
     try {
-        postCommandHandler(req.body, res);
+        const payload: PostCommandPayload = req.body;
+        const { team_id, command } = payload;
+        const slack_config = store.slackTeamConfig(team_id);
+        const slack = new Slack(slack_config);
+
+        if (command === '/support') {
+            support.handleCommand(slack, payload, res);
+        } else {
+            res.json({
+                text: 'Unknown or not implemented command.'
+            });
+        }
     } catch (error) {
         logger.error('postCommand', error);
         res.json({
