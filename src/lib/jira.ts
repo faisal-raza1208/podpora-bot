b8a5839ba9d5a6ba6b0dea49d1f5f1eb8635fa0f
Marek <marek@syftapp.com>
2021-07-31 16:15:31 +0100

    Minor improvements to jira module and tests (#503)
    * Remove dependencies from jira.test.ts
    
    why:
    to make easier to test and upgrade the jira package
    
    * Remove unused exported interface from jira.ts
    
    * Remove extra variable in jira.test.ts
    
    * Improve jira module specs

--- src/lib/jira.ts
+++ src/lib/jira.ts
@@ -91,6 +91,5 @@ class Jira {
 }
 
 export {
-    IssueParams,
     Jira
 };
--- test/lib/jira.test.ts
+++ test/lib/jira.test.ts
@@ -8,28 +8,9 @@ import {
 import {
     Issue,
 } from '../../src/lib/jira/api_interfaces';
-import supportConfig from '../../src/lib/support_config';
+
 const logErrorSpy = jest.spyOn(logger, 'error').mockReturnValue({} as Logger);
-
 const createIssueResponse = fixture('jira/issues.createIssue.response');
-
-const bug_report = {
-    id: '1592066203.000100',
-    team_id: 'THS7JQ2RL',
-    user: {
-        id: 'UHAV00MD0',
-        name: 'sherlock_holmes'
-    },
-    submission: {
-        title: 'bug report title',
-        description: 'bug report description',
-        expected: 'we like this to happen',
-        currently: 'this is what happens now',
-    },
-    url: 'http://example.com/bug',
-    channel: 'CHS7JQ7PY',
-    state: 'bug'
-};
 
 afterEach(() => {
     jest.clearAllMocks();
@@ -41,19 +22,23 @@ describe('Jira', () => {
         api_token: 'abc-123',
         host: 'http://example.com'
     };
-
     const jira = new Jira(mock_config);
-
     const issue = createIssueResponse as unknown as Issue;
+
     describe('#createIssue(params)', () => {
-        const submission = bug_report.submission;
-        const user = bug_report.user;
-        const request_type = 'bug';
-        const params = supportConfig('default')
-            .issueParams(submission, user, request_type);
+        const params = {
+            fields: {
+                project: { key: 'SUP' },
+                summary: 'Sample Summary',
+                issuetype: { name: 'Bug' },
+                description: 'Sample Description',
+                labels: ['some-label']
+            }
+        };
+
         it('returns a Promise that resolves to issue object', (done) => {
             let api_call_body: string;
-            expect.assertions(7);
+            expect.assertions(5);
             nock(mock_config.host)
                 .post('/rest/api/2/issue', (body) => {
                     api_call_body = JSON.stringify(body);
@@ -62,13 +47,10 @@ describe('Jira', () => {
 
             jira.createIssue(params)
                 .then((res) => {
-                    const submission = bug_report.submission;
                     expect(res).toEqual(issue);
-                    expect(api_call_body).toContain(submission.title);
-                    expect(api_call_body).toContain(submission.description);
-                    expect(api_call_body).toContain(submission.expected);
-                    expect(api_call_body).toContain(submission.currently);
-                    expect(api_call_body).toContain(user.name);
+                    expect(api_call_body).toContain('Sample Summary');
+                    expect(api_call_body).toContain('Sample Description');
+                    expect(api_call_body).toContain('some-label');
                     expect(api_call_body).toContain('Bug');
                     done();
                 }).catch(done);
@@ -102,6 +84,7 @@ describe('Jira', () => {
         const url = 'some random url';
 
         it('returns promise', (done) => {
+            expect.assertions(1);
             nock(mock_config.host)
                 .post(
                     `/rest/api/2/issue/${issue.key}/remotelink`
@@ -116,6 +99,7 @@ describe('Jira', () => {
 
         describe('api failure', () => {
             it('returns promise that rejects', (done) => {
+                expect.assertions(2);
                 nock(mock_config.host)
                     .post(
                         `/rest/api/2/issue/${issue.key}/remotelink`
@@ -131,17 +115,17 @@ describe('Jira', () => {
         });
     });
 
-    describe('#issueUrl', () => {
+    describe('#issueUrl(issue)', () => {
         it('returns url to jira issue on host domain', () => {
-            const issue = createIssueResponse as unknown as Issue;
             const url = jira.issueUrl(issue);
 
             expect(url).toEqual(`${mock_config.host}/browse/${issue.key}`);
         });
     });
 
-    describe('#addComment', () => {
+    describe('#addComment(issue_key, comment)', () => {
         it('logs the error', (done) => {
+            expect.assertions(4);
             nock('https://example.com')
                 .post('/rest/api/2/issue/issue-key/comment')
                 .reply(404);
