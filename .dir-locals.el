9dd0cb923f9517fdc57901b7b3ca87e17fc14981
Marek L <nospam.keram@gmail.com>
2022-09-18 12:16:51 +0100

    Update jest config
    Resolves:
    ```
    
    ts-jest[ts-jest-transformer] (WARN) Define `ts-jest` config under `globals` is deprecated.
    ```

---
+++ .dir-locals.el
@@ -0,0 +1,20 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((js2-mode
+    (js-indent-level . 4))
+ (js-mode
+    (js-indent-level . 4))
+ (json-mode
+    (js-indent-level . 2))
+ )
+
+;; ((nil . ((setq projectile-project-test-cmd "npx jest"))))
+;;          )))
+;; ((nil . (eval . (setq projectile-project-test-cmd "npx jest"))))
+
+;; ((projectile-register-project-type 'npm '("package.json")
+;;                                   :compile "npm install"
+;;                                   :test "npx jest"
+;;                                   :run "npm run start"
+;;                                   :test-suffix ".test"))
