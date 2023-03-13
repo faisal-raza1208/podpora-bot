0acffdbceacdc335ebdbe9d8f1f24882c9d5c857
emekafredy <emeka.s.chinedu@gmail.com>
2023-03-13 10:08:46 +0100

    add platform submissions jira components field

--- test/lib/support_config.test.ts
+++ test/lib/support_config.test.ts
@@ -185,6 +185,70 @@ Submitted by: ${slack_user.name}`;
                 });
             });
 
+            describe('bug with bug_report_with_components_field feature enabled', () => {
+                const request_type = 'bug';
+                const submission = {
+                    title: 'Test A',
+                    description: 'Test B',
+                    currently: 'Test C',
+                    expected: 'Test D',
+                    platform: 'Test E,Test Q',
+                    region: 'Test L',
+                    version: 'Test F',
+                    employer: 'Test G',
+                    worker: 'Test H',
+                    listing: 'Test I',
+                    shift: 'Test J',
+                    test_data: 'Test K',
+                    device: 'Test M',
+                    urgency: 'Test N',
+                    product_area: 'Test area'
+                };
+                const desc = `${submission.description}
+
+Currently:
+${submission.currently}
+
+Expected:
+${submission.expected}
+
+Product Area: ${submission.product_area}
+Urgent: ${submission.urgency}
+Platform/Device: ${submission.platform}
+Region/Country: ${submission.region}
+App version: ${submission.version}
+Employer ID: ${submission.employer}
+Worker ID: ${submission.worker}
+Listing ID: ${submission.listing}
+Shift ID: ${submission.shift}
+Test data: ${submission.test_data}
+
+Submitted by: ${slack_user.name}`;
+
+                it('matches expected object with product area included', () => {
+                    const featureSpy = jest.spyOn(feature, 'is_enabled');
+                    featureSpy.mockImplementationOnce(() => true)
+                              .mockImplementationOnce(() => true);
+
+                    expect(
+                        config.issueParams(submission, slack_user, request_type)
+                    ).toEqual({
+                        fields: {
+                            project: { key: 'SUP' },
+                            summary: submission.title,
+                            issuetype: { name: 'Bug' },
+                            description: desc,
+                            labels: ['support'],
+                            customfield_10773: { value: submission.product_area }, // "Flex Domain"
+                            components: [
+                                { name: 'Test E' },
+                                { name: 'Test Q' }
+                            ]
+                        }
+                    });
+                });
+            });
+
             describe('data', () => {
                 const request_type = 'data';
                 const submission = {
